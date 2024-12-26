0e0551bf0f0419b1bf1ad42c7618a0b65e64ab4e
Thomas Pfau <pfau.thomas@pro-tags.com>
2024-12-26 23:02:54 +0100

    Auto Update

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/PAF13/com_neondb
 
 go 1.23.4
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/PAF13/com_n26 v0.0.0-20241225175641-0549a3f244de // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+)
---
+++ wrapper_n26.go
@@ -0,0 +1,83 @@
+package com_neondb
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	n26 "github.com/PAF13/com_n26"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+)
+
+// prep a map to feed the batch sql
+func N26Upload(object []*n26.Transaction) {
+	n26BatchSend(valueReplace(object))
+}
+func valueReplace(object []*n26.Transaction) *[]string {
+	file, err := ioutil.ReadFile(`queries\INSERT_BankTransfer.sql`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	batch := []string{}
+
+	for _, v := range object {
+		insertSQL := string(file)
+		insertSQL = strings.Replace(insertSQL, "$1", "`"+v.BookingDate+"`", 1)
+		insertSQL = strings.Replace(insertSQL, "$2", "`"+v.ValueDate+"`", 1)
+		insertSQL = strings.Replace(insertSQL, "$3", "`"+v.PartnerName+"`", 1)
+		insertSQL = strings.Replace(insertSQL, "$4", "`"+v.PartnerIBAN+"`", 1)
+		insertSQL = strings.Replace(insertSQL, "$5", "`"+v.Type+"`", 1)
+		insertSQL = strings.Replace(insertSQL, "$6", "`"+v.PaymentReference+"`", 1)
+		insertSQL = strings.Replace(insertSQL, "$7", "`"+v.AccountName+"`", 1)
+		insertSQL = strings.Replace(insertSQL, "$8", fmt.Sprintf("%f", v.AmountEUR), 1)
+		insertSQL = strings.Replace(insertSQL, "$9", fmt.Sprintf("%f", v.OriginalAmount), 1)
+		insertSQL = strings.Replace(insertSQL, "$10", "`"+v.OriginalCurrency+"`", 1)
+		insertSQL = strings.Replace(insertSQL, "$11", fmt.Sprintf("%f", v.ExchangeRate), 1)
+
+		batch = append(batch, insertSQL)
+	}
+
+	return &batch
+}
+
+func n26BatchSend(commands *[]string) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	connStr := os.Getenv("DATABASE_URL")
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(context.Background())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 120*time.Second)
+	defer cancel()
+
+	// Create a batch
+	batch := &pgx.Batch{}
+
+	for _, b := range *commands {
+		batch.Queue(b)
+	}
+	// Send the batch
+	br := conn.SendBatch(ctx, batch)
+	defer br.Close()
+
+	// Handle batch results
+	for i := 0; i < len(*commands); i++ {
+		_, err := br.Exec()
+		if err != nil {
+			log.Printf("Error executing query %d: %v", i+1, err)
+		} else {
+			log.Printf("Query %d executed successfully.", i+1)
+		}
+	}
+}
