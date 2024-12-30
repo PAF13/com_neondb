9bcacfb15623518ef1c4ac982175c6713cba1677
Thomas Pfau <pfau.thomas@pro-tags.com>
2024-12-30 04:59:03 +0100

    Auto Update | Version: v0044

--- getTrans.go
+++ getTrans.go
@@ -72,7 +72,7 @@ func GetTrans() {
 
 	// Print the fetched records
 	for _, record := range records {
-		fmt.Printf("bookingdate: %10s partnername: %10s partneriban: %10s typess: %10s accountname: %10s\n",
+		fmt.Printf("bookingdate: %10s partnername: %20s partneriban: %10s typess: %10s accountname: %10s\n",
 			processPointer(record.bookingdate),
 			processPointer(record.partnername),
 			processPointer(record.partneriban),
